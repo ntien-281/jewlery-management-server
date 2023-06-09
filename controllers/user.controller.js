7069df1bc5fae2f803648f7786b741813a0bd72e
Hai Dinh <dinhnguyenminhhai0307@gmail.com>
2023-06-09 20:35:56 +0700

    check user authentication

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,25 +9,24 @@ const signup = async (req, res) => {
         const { username, password } = req.body;
         if(!username || !password)
             return res.status(400).send("Missing username or password.");
+        let token = jwt.sign({ username }, process.env.secretKey, {
+            expiresIn: process.env.expiresIn,
+        });
         const information = {
             username,
             password: await bcrypt.hash(password, 10),
+            token,
         };
         const user = await User.create(information);
         if(!user){
             return res.status(409).send("Sign up failed. Please try again.");
         }
-        let token = jwt.sign({ id: user.id }, process.env.secretKey, {
-            expiresIn: process.env.expiresIn,
-        });
-     
         res.cookie("jwt", token, { maxAge: process.env.cookieAge, httpOnly: true });
-        console.log("user", JSON.stringify(user, null, 2));
-        console.log(token);
-        return res.status(201).send(user);
+        return res.status(201).send({user, token});
     }
     catch(error){
         console.log(error);
+        return res.status(401).send(error);
     }
 }
 
@@ -48,16 +47,17 @@ const login = async (req, res) => {
         if(!isValid){
             return res.status(401).send("Wrong username or password");
         }
-        let token = jwt.sign({ id: user.id }, process.env.secretKey, {
+        let token = jwt.sign({ username }, process.env.secretKey, {
             expiresIn: process.env.expiresIn,
         });
+        user.token = token;
+        await user.save();
         res.cookie("jwt", token, { maxAge: process.env.cookieAge, httpOnly: true });
-        console.log("user", JSON.stringify(user, null, 2));
-        console.log(token);
-        return res.status(201).send(user);
+        return res.status(201).send({user, token});
     }
     catch(error){
         console.log(error);
+        return res.status(401).send(error);
     }
 }
 module.exports = { signup, login };
--- middleware/userauth.js
+++ middleware/userauth.js
@@ -1,11 +1,11 @@
 //importing modules
-const express = require("express");
+const jwt = require('jsonwebtoken')
 const db = require("../models");
 //Assigning db.users to User variable
 const User = db.User;
 
-//Function to check if username or email already exist in the database
-//this is to avoid having two users with the same username and email
+//Function to check if username already exist in the database
+//this is to avoid having two users with the same username
 const saveUser = async (req, res, next) => {
 //search the database to see if user exist
     try {
@@ -15,13 +15,38 @@ const saveUser = async (req, res, next) => {
             },
         });
         if (username) {
-            return res.json(409).send("Username already taken");
+            return res.status(409).send("Username already taken");
         }
         next();
     } catch (error) {
         console.log(error);
+        return res.status(401).send("Sign up failed.")
     }
 };
-
+const checkCookie = (req) => {
+    return req.cookies['jwt']
+}
+const verifyAccessToken = async (req, res, next) => {
+    let token;
+    //authenticate through Bearer token
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+        token = req.headers.authorization.split(' ')[1]
+    }
+    //authenticate through cookie
+    else {
+        token = req.headers["x-access-token"] || checkCookie(req) || null;
+    }
+    if (!token) {
+        return res.status(403).send("A token is required for authentication");
+    }
+    try {
+        const decodedUser = jwt.verify(token, process.env.secretKey);
+        req.user = decodedUser;
+    } catch (err) {
+        console.log(err);
+        return res.status(401).send("Invalid Token");
+    }
+    next();
+}
 //exporting module
-module.exports = { saveUser };
+module.exports = { saveUser, verifyAccessToken };
