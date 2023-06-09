1303ed7185a6dc0464083a11c9a8a6038b6b9f91
Hai Dinh <dinhnguyenminhhai0307@gmail.com>
2023-06-09 20:50:43 +0700

    supplier controller

---
+++ controllers/supplier.controller.js
@@ -0,0 +1,56 @@
+const db = require('../models')
+
+const Supplier = db.Supplier
+
+const getAllSuppliers = async (req, res) => {
+  const Suppliers = await Supplier.findAll();
+  if (Suppliers) {
+    if (Suppliers.length <= 0) res.status(200).send("No supplier found");
+    else {
+      res.status(200).send(Suppliers);
+    }
+  }
+}
+
+const createSupplier = async (req, res) => {
+  const { name, address, phone } = req.body;
+  let result;
+  if (!name || !phone) { res.status(400).send("Please provide name and phone number") };
+  result = await Supplier.findOne({
+    where : {
+        name: name
+    }
+  })
+  if(result){
+    return res.status(409).send("Supplier is already exists");
+  }
+  result = await Supplier.create({
+    name, address, phone
+  });
+  if (result) {
+    console.log("new Supplier created");
+    res.status(200).send(result);
+  }
+  else {
+    res.status(500).send("Something went wrong")
+  }
+}
+
+const updateSupplier = async (req, res) => {
+  const { name, address, phone } = req.body;
+  const updateId = req.params.id;
+  let Supplier = await Supplier.findByPk(updateId);
+  if (Supplier) {
+    Supplier.name = name ? name : Supplier.name;
+    Supplier.address = address ? address : Supplier.address;
+    Supplier.phone = phone ? phone : Supplier.phone;
+    await Supplier.save();
+    console.log(`Supplier ${updateId} updated succesfully.`);
+    res.status(200).send(Supplier);
+  }
+  else {
+    res.status(400).send("Supplier not found.");
+  }
+}
+
+module.exports = { getAllSuppliers, createSupplier, updateSupplier }
