2d5918338d740bf46e3f18ba2530666395f0c055
nhattien <nhattien1245@gmail.com>
2023-06-13 22:42:05 +0700

    fix enabling cors, cookies header

--- server.js
+++ server.js
@@ -67,6 +67,13 @@ app.use(cors({
   origin: process.env.FE_ORIGIN,
   credentials: true
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.FE_ORIGIN);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(cookieParser());
 
 
