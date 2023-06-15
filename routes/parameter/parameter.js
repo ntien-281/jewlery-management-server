c96a5490e7134dfa7815d25c882bd64f91a2d8c1
Hai Dinh <dinhnguyenminhhai0307@gmail.com>
2023-06-15 12:35:59 +0700

    fix parameter routes

--- routes/parameter/parameter.js
+++ routes/parameter/parameter.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const parameterController = require('../../controllers/parameter.controller')
 //const checkUserPermission = require("../../middleware/authorization").checkUserPermission
 
-app.get("/parameter", parameterController.getParameter)
-app.post("/parameter", parameterController.createParameter)
-app.put("/parameter/:id", parameterController.updateParameter)
-//app.get("/parameter", parameterController.getParameter)
-//app.post("/parameter", checkUserPermission('change'), parameterController.createParameter)
-//app.put("/parameter/:id", checkUserPermission('change'), parameterController.updateParameter)
+router.get("/parameter", parameterController.getParameter)
+router.post("/parameter", parameterController.createParameter)
+router.put("/parameter/:id", parameterController.updateParameter)
+//router.get("/parameter", parameterController.getParameter)
+//router.post("/parameter", checkUserPermission('change'), parameterController.createParameter)
+//router.put("/parameter/:id", checkUserPermission('change'), parameterController.updateParameter)
+module.exports = router;
